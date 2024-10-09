5e0da087b266083abcb82f19afddea0ca6dc5dea
Raykesh_Soneka <raykesh.ravirooban@soneka.cloud>
2024-10-09 14:42:27 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   Cyber/Red Team/privateRed.html
    	modified:   Cyber/Red Team/privateRed.js
    	modified:   Cyber/Red Team/publicRed.html
    	modified:   Cyber/Red Team/publicRed.js

--- Cyber/Red Team/privateRed.js
+++ Cyber/Red Team/privateRed.js
@@ -1,2 +1,3 @@
+import * as nom2 from "../jsencrypt.min.js";
 var decrypt = new JSEncrypt();
 decrypt.setPrivateKey(document.getElementById("privkeyRed").value);
--- Cyber/Red Team/publicRed.js
+++ Cyber/Red Team/publicRed.js
@@ -1,2 +1,3 @@
+import * as nom2 from "../jsencrypt.min.js";
 var encrypt = new JSEncrypt();
 encrypt.setPublicKey(document.getElementById("pubkeyRed").value);
