684d1585423a3f59cfae417bfc370e29207cdf6b
Raykesh_Soneka <raykesh.ravirooban@soneka.cloud>
2024-10-09 14:47:00 +0200

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   Cyber/Red Team/privateRed.js
    	modified:   Cyber/Red Team/publicRed.js

--- Cyber/Red Team/publicRed.js
+++ Cyber/Red Team/publicRed.js
@@ -1,3 +1,16 @@
 import * as nom2 from "../jsencrypt.min.js";
 var encrypt = new JSEncrypt();
-encrypt.setPublicKey(document.getElementById("pubkeyRed").value);
+encrypt.setPublicKey("-----BEGIN PUBLIC KEY-----\n"+
+"MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAsnsWwp7qNrXklcRtLTSD\n"+
+"5hIyJ00JjEp3tWhoFzD0Ah+yG3TqK7yNul443bEsl0HZ1CndzoXFgmxXs33ySVOT\n"+
+"FnSi1yyF6GMw4Hpr1u1Evvd8hsAP9ezAny7f+GSSPL0mI8YwI2rCNm2UVWJvEWH+\n"+
+"zQOz02WAyvbE1zLr8aKAHHee2ubsaM0A8t/IOjElV/mwfGQnM45prrgDtBEuFuPi\n"+
+"tgkzL1YOimT6TqyCtgcXypsg8c6vTXBhzOlY5J8bh/7pyYZUKOAaQwT6+7rQqTyd\n"+
+"dKdmcP08kmpXutwaHQjsImTce8NlbBvlYZXOHZmBphg6x9POUGOB+VP2LP5KxZvX\n"+
+"Y0/BH50Ma/1nC53Nbsb7GswgE+fPJV0WJWtS46CEnMOt3WQjr4wz/uq4+hZKf8LG\n"+
+"N6RfIV864KWYEZpUNoRO9B8CdbumcLOTQbOWNOgHBOfa3dXJpIWfnZHPnyqhAQ+b\n"+
+"9fun4sWCY1iO2Zsjh3pxugCrFRcx9XQOfF3sUEJKAxaBd0TjBc1sYxm1xbW0lbmR\n"+
+"r4Hi6tpqywdNYpbl2M8PKvON1hRt5MQ4PWXUBiHb7OFgxt6BHP7XxEZtXVxptgZn\n"+
+"9Nj9l1yAMTEFVkUHF/BqeNUR/Lr5Ddbe4u3LLbTqZAhWAIyeBr54Iwmp8HKiZCGV\n"+
+"8j0FhGOnQYRlR+e4hPexo/ECAwEAAQ==\n"+
+"-----END PUBLIC KEY-----");
