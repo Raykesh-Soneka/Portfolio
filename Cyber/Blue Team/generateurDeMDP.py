7fedfb8c2be6a7038efb4e1ec1a528cf1a2e6e5d
Raykesh_Soneka <raykesh.ravirooban@soneka.cloud>
2024-10-29 10:33:49 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   Cyber/Blue Team/generateurDeMDP.py

--- Cyber/Blue Team/generateurDeMDP.py
+++ Cyber/Blue Team/generateurDeMDP.py
@@ -1,6 +1,14 @@
 import string,secrets
 
 Lettre = string.ascii_letters
+Espace = string.whitespace
+nombre = string.digits
+hexa = string.hexdigits
+octa = string.octdigits
+ponctuation = string.punctuation
+tout = string.printable
+print(string.__doc__)
+print(['\n'+s for s in [Lettre, Espace, nombre, hexa, octa, ponctuation, tout]])
 
 Charset : string = Lettre
 
