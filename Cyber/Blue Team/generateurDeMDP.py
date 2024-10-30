4711cedf93d09b1083c3f15471232811f9bd37bc
Raykesh_Soneka <raykesh.ravirooban@soneka.cloud>
2024-10-30 15:36:08 +0100

    Refacto On branch main Your branch is ahead of 'origin/main' by 4 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   Cyber/Blue Team/generateurDeMDP.py

--- Cyber/Blue Team/generateurDeMDP.py
+++ Cyber/Blue Team/generateurDeMDP.py
@@ -21,9 +21,8 @@ if __debug and ___debug > 8:print(len(Charset))
 #ici le charset est plutôt court (100 caractères)
 #Il faut modifier le code pour pouvoir avoir un plus grand charset (avec utf8 mais ces caractère ne sont pas pris TOUJOURS en charge)
 
-def generateurDeMotsDePasse(n : int = 0 , Charset : string = Charset ) -> None:
-    if n == 0:
-        int(input(" Entrer le Nombre de caractère (longueur) pour génèré un mots de passe : "))
+def generateurDeMotsDePasse(n : int = 0 , Charset : string = Charset ) -> string:
+    if n == 0:int(input(" Entrer le Nombre de caractère (longueur) pour génèré un mots de passe : "))
     # MDP : string = ""
     # for i in range(n):MDP += secrets.choice(Charset)
     # return MDP
@@ -35,4 +34,4 @@ def generateurDeMotsDePasse(n : int = 0 , Charset : string = Charset ) -> None:
 if __debug:
     if ___debug > 7 :
         print(Charset)
-    print(generateurDeMotsDePasse(50))
+    print(generateurDeMotsDePasse(50,Charset))
