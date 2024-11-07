5a7f36a13431648730b1d4ea9fa761e66751a00c
Raykesh_Soneka <raykesh.ravirooban@soneka.cloud>
2024-11-07 14:31:42 +0100

    On branch main Your branch is ahead of 'origin/main' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	new file:   Cyber/Blue Team/chiffreur.py

---
+++ Cyber/Blue Team/chiffreur.py
@@ -0,0 +1,28 @@
+
+Folder = "Cyber\\Blue Team\\"
+key = 0x12  # Exemple de clée pour le XOR
+
+def xor_donnee(data, key):
+    return bytes(b ^ key for b in data)
+
+with open(Folder+"generateurDeMDP.py","r+b") as o :
+    with open(Folder+"generateurDeMDPChiffrer.py",'w+b') as c :
+        # for i in o:
+        #     c.write(i)
+        while True:
+            morceaux = o.read(1024)  # lis des morceaux de 1024 octets
+            if not morceaux:
+                break
+            morceaux_chiffrer = xor_donnee(morceaux, key)  # appliquer le XOR
+            c.write(morceaux_chiffrer)
+
+with open(Folder+"generateurDeMDPChiffrer.py","r+b") as c :
+    with open(Folder+"generateurDeMDPDéChiffrer.py",'x+b') as d :
+        # for i in o:
+        #     d.write(i)
+        while True:
+            morceaux = c.read(1024)  # lis des morceaux de taile 1024 octets
+            if not morceaux:
+                break
+            morceaux_chiffrer = xor_donnee(morceaux, key)  # appliquer le XOR
+            d.write(morceaux_chiffrer)
