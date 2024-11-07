400a1cffb0b77a4e9a4552e1d368cf5f4b6ede18
Raykesh_Soneka <raykesh.ravirooban@soneka.cloud>
2024-11-07 14:37:31 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   Cyber/Blue Team/chiffreur.py

--- Cyber/Blue Team/chiffreur.py
+++ Cyber/Blue Team/chiffreur.py
@@ -5,6 +5,7 @@ key = 0x12  # Exemple de clée pour le XOR
 def xor_donnee(data, key):
     return bytes(b ^ key for b in data)
 
+## Créé un fichier chiffrer
 with open(Folder+"generateurDeMDP.py","r+b") as o :
     with open(Folder+"generateurDeMDPChiffrer.py",'w+b') as c :
         # for i in o:
@@ -14,6 +15,8 @@ with open(Folder+"generateurDeMDP.py","r+b") as o :
             if not morceaux:break
             c.write(xor_donnee(morceaux, key)  )  # appliquer le XOR / écrire ds le fichier
 
+Texte:str=""
+## Déchiffre le fichier chiffrer
 with open(Folder+"generateurDeMDPChiffrer.py","r+b") as c :
     with open(Folder+"generateurDeMDPDéChiffrer.py",'x+b') as d :
         # for i in o:
