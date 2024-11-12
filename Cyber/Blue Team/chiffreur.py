2c02f2a8e52a040707a57a472aafa8548078470f
Raykesh_Soneka <raykesh.ravirooban@soneka.cloud>
2024-11-12 11:21:49 +0100

    On branch main Your branch is ahead of 'origin/main' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   Cyber/Blue Team/chiffreur.py

--- Cyber/Blue Team/chiffreur.py
+++ Cyber/Blue Team/chiffreur.py
@@ -27,7 +27,7 @@ with open(Folder+"generateurDeMDPChiffrer.py","r+b") as c :
             if not morceaux:break
             d.write(xor_donnee(morceaux, key)  )  # appliquer le XOR / écrire ds le fichier
             Texte+=str(xor_donnee(morceaux, key))#.replace("\\r\\n",'\n')
-    import generateurDeMDPDéChiffrer
+    import generateurDeMDPDéChiffrer # type: ignore
 
 # eval(Texte)
 
