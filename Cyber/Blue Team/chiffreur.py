15f47616ccdd178c1f777b72aede06cd68aeb070
Raykesh_Soneka <raykesh.ravirooban@soneka.cloud>
2024-11-12 11:26:34 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   Cyber/Blue Team/chiffreur.py
    	modified:   Cyber/Blue Team/generateurDeMDPChiffrer.py

--- Cyber/Blue Team/chiffreur.py
+++ Cyber/Blue Team/chiffreur.py
@@ -28,7 +28,12 @@ with open(Folder+"generateurDeMDPChiffrer.py","r+b") as c :
             d.write(xor_donnee(morceaux, key)  )  # appliquer le XOR / écrire ds le fichier
             Texte+=str(xor_donnee(morceaux, key))#.replace("\\r\\n",'\n')
     import generateurDeMDPDéChiffrer,os # type: ignore
-    os.remove("generateurDeMDPDéChiffrer")
+    try:
+        os.remove("generateurDeMDPDéChiffrer.py")
+        os.rmdir("__pycache__")
+    except:
+        os.remove("Cyber\Blue Team\generateurDeMDPDéChiffrer.py")
+        os.rmdir("Cyber\Blue Team\__pycache__")
 
 # eval(Texte)
 
